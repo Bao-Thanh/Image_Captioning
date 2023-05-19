d0d830cab5bf5b1288a8d2d820b31f3a2f610bbb
Bao-Thanh <baothanhbtlh@gmail.com>
2023-05-20 01:03:28 +0700

    Update FE

--- FE/Image Captioning FE - Angular/src/app/home/home.page.ts
+++ FE/Image Captioning FE - Angular/src/app/home/home.page.ts
@@ -2,6 +2,8 @@ import { CaptionService } from './../Services/caption.service';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Camera } from '@ionic-native/camera/ngx';
 import { Platform } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 import {
   Plugins,
   Capacitor,
@@ -34,11 +36,13 @@ export class HomePage {
   caption: string;
   captionGenerated: any;
   resultGenerated: any;
- 
+  searchGenerated: any;
+  searchText: any;
 
   constructor(
     private camera: Camera,
     private platform: Platform,
+    private toastController: ToastController,
     private captionService: CaptionService
   ) {}
 
@@ -64,6 +68,8 @@ export class HomePage {
   }
 
   captureImageFromCamera() {
+    this.searchGenerated = false;
+
     if (Capacitor.isPluginAvailable('Camera')) {
       this.capTureImageOnDesktop();
       return;
@@ -85,21 +91,9 @@ export class HomePage {
     }
   }
 
-  captureImageFromGallery() {
-    if (this.platform.is("cordova")) {
-      this.camera.getPicture({
-        sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
-        destinationType: this.camera.DestinationType.DATA_URL
-      }).then((res) => {
-        this.imgUrl = 'data:image/jpeg;base64,' + res;
-        this.captionGenerated = false;
-      }).catch((error) => {
-        console.log(error);
-      });
-    } else {
-      console.log(this.filePickerRef);
-      this.filePickerRef.nativeElement.click();
-    }
+  searchCaption() {
+    this.searchGenerated = true
+
   }
 
   onFileChosen(event: Event) {
@@ -117,22 +111,12 @@ export class HomePage {
     fr.readAsDataURL(pickedFile);
   }
 
-  async generateCaption() {
+  async generateCaption_view() {
     if(!this.imgUrl) { return; }
     this.captionGenerated = true;
     this.resultGenerated = false;
 
     let img_path
-    // if (typeof this.imgUrl === 'string') {
-    //   try {
-    //     imageFile = dataURLtoFile(this.imgUrl, 'caption-img.jpeg');        
-    //   } catch (error) {
-    //     console.log(error);
-    //     return;
-    //   }
-    // } else {
-    //   imageFile = this.imgUrl;
-    // }
 
     img_path = this.imgUrl;
 
@@ -152,38 +136,77 @@ export class HomePage {
       )
   }
 
-  // async generateOTP() {
-  //   if(!this.imgUrl) { return; }
-  //   this.captionGenerated = true;
+  async generateCaption_read() {
+    if (!this.caption) {
+      return;
+    }
+  
+    try {
+      // Check if the platform is desktop
+      if (!this.platform.is('cordova')) {
+        if ('speechSynthesis' in window) {
+          const utterance = new SpeechSynthesisUtterance(this.caption);
+          utterance.lang = 'en-US'; // Set the language to English
+          speechSynthesis.speak(utterance);
+        } else {
+          console.log('Text-to-speech is not supported on this platform.');
+        }
+      } else {
+        console.log('Text-to-speech is not supported on mobile platforms.');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+  async copyCaption() {
+    if (!this.caption) {
+      return;
+    }
+  
+    try {
+      if (this.platform.is('cordova')) {
+        // Copy functionality is not supported on mobile platforms
+        console.log('Copying to clipboard is not supported on mobile platforms.');
+        return;
+      }
+  
+      const textarea = document.createElement('textarea');
+      textarea.value = this.caption;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+  
+      console.log('Caption copied to clipboard');
+  
+      const toast = await this.toastController.create({
+        message: 'Caption copied to clipboard',
+        duration: 2000, // Adjust the duration as needed
+        position: 'bottom' // Set the toast position
+      });
+      toast.present();
+    } catch (error) {
+      console.log('Error copying caption to clipboard:', error);
+      // Handle error or show error message to the user
+    }
+  }
+  
+  onSearchChange(event: any) {
+    const searchTerm = event.detail.value;
+    // Perform search based on the entered search term
+    // Add your search logic here
+    console.log('Search term:', searchTerm);
+  }
 
-  //   let img_path
-
-  //   // if (typeof this.imgUrl === 'string') {
-  //   //   try {
-  //   //     imageFile = dataURLtoFile(this.imgUrl, 'caption-img.jpeg');        
-  //   //   } catch (error) {
-  //   //     console.log(error);
-  //   //     return;
-  //   //   }
-  //   // } else {
-  //   //   imageFile = this.imgUrl;
-  //   // }
-
-  //   img_path = this.imgUrl;
-
-  //   this.captionService.generateOTP(
-  //     img_path
-  //   )
-  //     .subscribe(
-  //       (response) => {
-  //         console.log(response);
-  //       }, (error) => {
-  //         console.log(error);
-  //       }
-  //     )
-  // }
-
+  performSearch() {
+    // Perform search based on the current value of searchText
+    // Add your search logic here
+    console.log('Performing search:', this.searchText);
+  }
+  
   hideCaption() {
     this.captionGenerated = false;
+    this.searchGenerated = false;
   }
 }
